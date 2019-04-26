cbb056e14878926f9a8f5d630b733da343dc4eba
Nikki Petersen <piknik@me.com>
2019-04-26 09:33:46 -0600

    added some handle functions

--- src/App.js
+++ src/App.js
@@ -9,16 +9,68 @@ import friends from "./friends"
 
 class App extends Component {
   state = {
-    friends: friends
+    friends: friends,
+    score: 0,
+    topScore: 0,
+    alreadyChosenIds: [],
+    correct: ""
   }
 
-  handleSelect = (id) => {
-    // this is where we shuffle the cards NOT selected
-    // need to fix this so it shuffles them ******************************
-    // ********************************
-    const remainingFriends = this.state.friends.filter(friend => (friend.id !== id));
-    this.setState({friends: remainingFriends})
+  // thank you, google, for this shuffle thing
+  //FISHER YATES SORTING FORMULA FOR SHUFFLING AN ARRAY
+  shuffle = friendArray => {
+    var i = 0
+        , j = 0
+        , temp = null
+
+    for (i = friendArray.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = friendArray[i]
+      friendArray[i] = friendArray[j]
+      friendArray[j] = temp
+    }
+    // console.log("NEW ARRAY")
+    return friendArray
+
   }
+
+
+  handleSelect = (id) => {
+    if (this.state.alreadyChosenIds.indexOf(id) === 1) { // if the specific id is not in the array of chosen ids
+      this.handleScoring(); // run this function right now
+      this.setState({alreadyChosenIds: this.state.alreadyChosenIds.concat(id)}); // add this id
+    } else {
+      if (this.state.alreadyChosenIds.indexOf(id) >= 0) {
+        this.setState({correct: "Doh!  You clicked something twice!  New game..."});
+        this.handleEndOfGame();
+      }
+    }
+  }
+
+  handleScoring = () => {
+    this.setState({correct: "Yeah Boiiiiii!"});
+    const theScore = this.state.score + 1; // not directly setting state
+    this.setState({score: theScore}) // set the score to theScore
+    if (theScore > this.state.topScore) {
+      this.setState({topScore: theScore}); // if your score is higher than the top score, reset the top score
+    } else if (this.state.score === 12) {
+      alert("wow! good job!")
+    }
+    this.setState({friends: this.shuffle(this.state.friends)}) // if it makes it this far, shuffle the images
+  }
+
+
+  handleEndOfGame = () => {
+    // reset the state
+    this.setState({
+      score: 0,
+      topScore: this.state.topScore,
+      alreadyChosenIds: [],
+    });
+    this.setState({ friends: this.shuffle(this.state.friends) }) // reset the state to shuffle the images
+  }
+
+
 
   render() {
     return (
