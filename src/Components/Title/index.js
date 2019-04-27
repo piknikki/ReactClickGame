91560275170b45696fd35b3c681e16b87118b128
Nikki Petersen <piknik@me.com>
2019-04-27 08:23:09 -0600

    fixing borders on title

--- src/Components/Title/index.js
+++ src/Components/Title/index.js
@@ -6,7 +6,7 @@ const Title = props => (
         <div className="col-sm-6">
             <img id="title-image" src={process.env.PUBLIC_URL + '/images/camplogo2.png'} alt="camp logo" />
         </div>
-        <div className="col-sm-6">
+        <div className="col-sm-5">
             <p>Directions: Click on any image. After you click, the images will shuffle. Don't click on anything more than once.
             </p>
         </div>
